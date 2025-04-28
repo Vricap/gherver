6a73145f9c187fc20e021d878450704fb060ccb0
Vricap <vricagedepenggalih@gmail.com>
2025-04-28 18:49:26 +0700

    tcp con running

--- main.go
+++ main.go
@@ -2,12 +2,53 @@ package main
 
 import (
 	"fmt"
-	// "net"
+	"net"
 )
+
+const ADDR string = "127.0.0.1"
+const PORT string = ":8000"
 
 func main() {
 	// listen for incoming connections on port 8000
-	// ln, err := net.Lis
-	fmt.Println("Helo world")
+	ln, err := net.Listen("tcp", ADDR+PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("TCP listening on", ADDR+PORT)
 
+	// accept incoming connections and handle them
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// handle the connections in a new goroutine
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	// close the connection when we're done
+	defer conn.Close()
+
+	// read incoming data
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// send back the data
+	_, err = conn.Write(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// print the incoming data
+	fmt.Printf("Received: %s", buf)
 }
