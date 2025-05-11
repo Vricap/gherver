ff646ba61641d533a978784e19ca1c3af1948f20
Vricap <vricagedepenggalih@gmail.com>
2025-05-11 19:52:12 +0700

    add default response header

--- http/http.go
+++ http/http.go
@@ -3,6 +3,7 @@ package http
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"time"
 )
 
@@ -21,7 +22,7 @@ func (r *Response) join() []byte {
 	return rs
 }
 
-type res_headers struct {
+type res_headers struct { // use MAP...
 	response_line []byte
 	server        []byte
 	date          []byte
@@ -29,7 +30,7 @@ type res_headers struct {
 	cont_length   []byte
 }
 
-func (h *res_headers) join() []byte { // use MAP...
+func (h *res_headers) join() []byte {
 	r := []byte{}
 	r = append(r, h.response_line...)
 	r = append(r, h.server...)
@@ -38,6 +39,99 @@ func (h *res_headers) join() []byte { // use MAP...
 	r = append(r, h.cont_length...)
 	return r
 }
+
+func NewResponseHeader() *res_headers {
+	y, m, d := time.Now().Date()
+
+	// default header
+	header := &res_headers{
+		response_line: []byte("HTTP/1.1 200 OK" + "\r\n"),
+		server:        []byte("Server: " + "Vricap" + "\r\n"),
+		date:          []byte("Date: " + fmt.Sprintf("%v %s %v", y, m, d) + "\r\n"),
+		cont_type:     []byte("Content-Type: " + "text/html" + "\r\n"),
+		cont_length:   []byte("Content-Length: " + "69" + "\r\n"),
+	}
+	return header
+}
+
+func (h *res_headers) responseLine(code int) {
+	StatusCodes := map[int]string{
+		// 1xx: Informational
+		100: "Continue",
+		101: "Switching Protocols",
+		102: "Processing",
+		103: "Early Hints",
+
+		// 2xx: Success
+		200: "OK",
+		201: "Created",
+		202: "Accepted",
+		203: "Non-Authoritative Information",
+		204: "No Content",
+		205: "Reset Content",
+		206: "Partial Content",
+		207: "Multi-Status",
+		208: "Already Reported",
+		226: "IM Used",
+
+		// 3xx: Redirection
+		300: "Multiple Choices",
+		301: "Moved Permanently",
+		302: "Found",
+		303: "See Other",
+		304: "Not Modified",
+		305: "Use Proxy",
+		307: "Temporary Redirect",
+		308: "Permanent Redirect",
+
+		// 4xx: Client Error
+		400: "Bad Request",
+		401: "Unauthorized",
+		402: "Payment Required",
+		403: "Forbidden",
+		404: "Not Found",
+		405: "Method Not Allowed",
+		406: "Not Acceptable",
+		407: "Proxy Authentication Required",
+		408: "Request Timeout",
+		409: "Conflict",
+		410: "Gone",
+		411: "Length Required",
+		412: "Precondition Failed",
+		413: "Payload Too Large",
+		414: "URI Too Long",
+		415: "Unsupported Media Type",
+		416: "Range Not Satisfiable",
+		417: "Expectation Failed",
+		418: "I'm a teapot",
+		421: "Misdirected Request",
+		422: "Unprocessable Entity",
+		423: "Locked",
+		424: "Failed Dependency",
+		425: "Too Early",
+		426: "Upgrade Required",
+		428: "Precondition Required",
+		429: "Too Many Requests",
+		431: "Request Header Fields Too Large",
+		451: "Unavailable For Legal Reasons",
+
+		// 5xx: Server Error
+		500: "Internal Server Error",
+		501: "Not Implemented",
+		502: "Bad Gateway",
+		503: "Service Unavailable",
+		504: "Gateway Timeout",
+		505: "HTTP Version Not Supported",
+		506: "Variant Also Negotiates",
+		507: "Insufficient Storage",
+		508: "Loop Detected",
+		510: "Not Extended",
+		511: "Network Authentication Required",
+	}
+	h.response_line = []byte("HTTP/1.1 " + strconv.Itoa(code) + " " + StatusCodes[code] + "\r\n")
+}
+
+// func (h *res_headers)
 
 type res_body struct {
 	body []byte
@@ -59,15 +153,9 @@ func HandleConnection(conn net.Conn) {
 		return
 	}
 
-	y, m, d := time.Now().Date()
 	// send back the data
-	header := &res_headers{
-		response_line: []byte("HTTP/1.1 200 OK" + "\r\n"),
-		server:        []byte("Server: " + "Monzo" + "\r\n"),
-		date:          []byte("Date: " + fmt.Sprintf("%v %s %v", y, m, d) + "\r\n"),
-		cont_type:     []byte("Content-Type: " + "text/html" + "\r\n"),
-		cont_length:   []byte("Content-Length: " + "69" + "\r\n"),
-	}
+	header := NewResponseHeader()
+	header.responseLine(404)
 	body := &res_body{
 		// body: []byte("Helo World!" + "\r\n"),
 		// body: []byte("<html><body><h1>Helo world!</h1></body></html>"),
