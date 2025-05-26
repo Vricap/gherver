b8e8d5c52dd20f0955b6106c2966a3a2ff3fc075
Vricap <vricagedepenggalih@gmail.com>
2025-05-26 09:09:24 +0700

    refactor

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
-	"github.com/vricap/gherver/http"
+	"github.com/vricap/gherver/http" // import the http package
 )
 
 const ADDR string = "127.0.0.1"
 const PORT string = ":8000"
 
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	City string `json:"city"`
+}
+
 func main() {
 	h := http.Init()
-	h.LoadStatic("/static", "./resource")
+	h.LoadStatic("/static", "./resource") // load static file
 
 	h.Routes = []*http.Routes{
 		{
@@ -23,8 +31,8 @@ func main() {
 				h.Response.Headers.ContType = "text/html"
 
 				// set response body
-				h.Response.SendDocument("./resource/index.html")
-				// h.Response.SendMedia("./resource/gophers.png")
+				h.Response.SendDocument("./resource/index.html") // send a document
+				// h.Response.SendMedia("./resource/gophers.png") // or a media
 			},
 		},
 		{
@@ -33,7 +41,18 @@ func main() {
 			Handler: func(h *http.Http) {
 				h.Response.Headers.SetStatusCode(200)
 				h.Response.Headers.ContType = "application/json"
-				h.Response.Body.Byte([]byte(`{"user_id": 1, "message": "Hello world"}`))
+
+				// send a json by struct
+				person := Person{Name: "John Doe", Age: 30, City: "New York"}
+				jsonData, err := json.Marshal(person)
+				if err != nil {
+					log.Fatalf("Error marshaling JSON: %s", err)
+				}
+				h.Response.Body.Byte(jsonData)
+
+				// or directly like this
+				// h.Response.Body.Byte([]byte(`{"user_id": 1, "message": "Hello world"}`))
+
 			},
 		},
 	}
